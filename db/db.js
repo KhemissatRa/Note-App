decf19369294689bafbf8e0f4a9fe773218bec98
khemissat <khemissat.ra@gmail.com>
2025-12-02 20:17:02 +0100

    fixing DB Issue

--- db/db.js
+++ db/db.js
@@ -4,8 +4,7 @@ const mongoose = require("mongoose");
 async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+     
     });
     console.log("✅ MongoDB Connected via Mongoose");
   } catch (err) {
